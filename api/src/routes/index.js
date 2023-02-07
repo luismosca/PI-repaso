feec5f54d2fc9ce324fa4b1957f6dc5a51a8f8f9
luismosca <luismosca@gmail.com>
2023-02-07 18:07:28 -0300

    un cambio en api

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,6 +7,6 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+// Aqui debemos confiruar las rutas que vamos a usar
 
 module.exports = router;
